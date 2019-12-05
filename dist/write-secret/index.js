35be136645501846644870214890069010b1499a
Taiwo Orilogbon <taiwo@bundle.africa>
2019-12-05 10:54:07 +0100

    Completed write action.

---
+++ dist/write-secret/index.js
@@ -0,0 +1,476 @@
+module.exports =
+/******/ (function(modules, runtime) { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	__webpack_require__.ab = __dirname + "/";
+/******/
+/******/ 	// the startup function
+/******/ 	function startup() {
+/******/ 		// Load entry module and return exports
+/******/ 		return __webpack_require__(691);
+/******/ 	};
+/******/
+/******/ 	// run startup
+/******/ 	return startup();
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 68:
+/***/ (function(__unusedmodule, exports) {
+
+exports.secretStoreUrl = 'https://v41enl2oo6.execute-api.us-east-2.amazonaws.com/default/secretstore';
+
+
+/***/ }),
+
+/***/ 87:
+/***/ (function(module) {
+
+module.exports = require("os");
+
+/***/ }),
+
+/***/ 211:
+/***/ (function(module) {
+
+module.exports = require("https");
+
+/***/ }),
+
+/***/ 417:
+/***/ (function(module) {
+
+module.exports = require("crypto");
+
+/***/ }),
+
+/***/ 431:
+/***/ (function(__unusedmodule, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+const os = __webpack_require__(87);
+/**
+ * Commands
+ *
+ * Command Format:
+ *   ##[name key=value;key=value]message
+ *
+ * Examples:
+ *   ##[warning]This is the user warning message
+ *   ##[set-secret name=mypassword]definitelyNotAPassword!
+ */
+function issueCommand(command, properties, message) {
+    const cmd = new Command(command, properties, message);
+    process.stdout.write(cmd.toString() + os.EOL);
+}
+exports.issueCommand = issueCommand;
+function issue(name, message = '') {
+    issueCommand(name, {}, message);
+}
+exports.issue = issue;
+const CMD_STRING = '::';
+class Command {
+    constructor(command, properties, message) {
+        if (!command) {
+            command = 'missing.command';
+        }
+        this.command = command;
+        this.properties = properties;
+        this.message = message;
+    }
+    toString() {
+        let cmdStr = CMD_STRING + this.command;
+        if (this.properties && Object.keys(this.properties).length > 0) {
+            cmdStr += ' ';
+            for (const key in this.properties) {
+                if (this.properties.hasOwnProperty(key)) {
+                    const val = this.properties[key];
+                    if (val) {
+                        // safely append the val - avoid blowing up when attempting to
+                        // call .replace() if message is not a string for some reason
+                        cmdStr += `${key}=${escape(`${val || ''}`)},`;
+                    }
+                }
+            }
+        }
+        cmdStr += CMD_STRING;
+        // safely append the message - avoid blowing up when attempting to
+        // call .replace() if message is not a string for some reason
+        const message = `${this.message || ''}`;
+        cmdStr += escapeData(message);
+        return cmdStr;
+    }
+}
+function escapeData(s) {
+    return s.replace(/\r/g, '%0D').replace(/\n/g, '%0A');
+}
+function escape(s) {
+    return s
+        .replace(/\r/g, '%0D')
+        .replace(/\n/g, '%0A')
+        .replace(/]/g, '%5D')
+        .replace(/;/g, '%3B');
+}
+//# sourceMappingURL=command.js.map
+
+/***/ }),
+
+/***/ 470:
+/***/ (function(__unusedmodule, exports, __webpack_require__) {
+
+"use strict";
+
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const command_1 = __webpack_require__(431);
+const os = __webpack_require__(87);
+const path = __webpack_require__(622);
+/**
+ * The code to exit an action
+ */
+var ExitCode;
+(function (ExitCode) {
+    /**
+     * A code indicating that the action was successful
+     */
+    ExitCode[ExitCode["Success"] = 0] = "Success";
+    /**
+     * A code indicating that the action was a failure
+     */
+    ExitCode[ExitCode["Failure"] = 1] = "Failure";
+})(ExitCode = exports.ExitCode || (exports.ExitCode = {}));
+//-----------------------------------------------------------------------
+// Variables
+//-----------------------------------------------------------------------
+/**
+ * sets env variable for this action and future actions in the job
+ * @param name the name of the variable to set
+ * @param val the value of the variable
+ */
+function exportVariable(name, val) {
+    process.env[name] = val;
+    command_1.issueCommand('set-env', { name }, val);
+}
+exports.exportVariable = exportVariable;
+/**
+ * exports the variable and registers a secret which will get masked from logs
+ * @param name the name of the variable to set
+ * @param val value of the secret
+ */
+function exportSecret(name, val) {
+    exportVariable(name, val);
+    // the runner will error with not implemented
+    // leaving the function but raising the error earlier
+    command_1.issueCommand('set-secret', {}, val);
+    throw new Error('Not implemented.');
+}
+exports.exportSecret = exportSecret;
+/**
+ * Prepends inputPath to the PATH (for this action and future actions)
+ * @param inputPath
+ */
+function addPath(inputPath) {
+    command_1.issueCommand('add-path', {}, inputPath);
+    process.env['PATH'] = `${inputPath}${path.delimiter}${process.env['PATH']}`;
+}
+exports.addPath = addPath;
+/**
+ * Gets the value of an input.  The value is also trimmed.
+ *
+ * @param     name     name of the input to get
+ * @param     options  optional. See InputOptions.
+ * @returns   string
+ */
+function getInput(name, options) {
+    const val = process.env[`INPUT_${name.replace(/ /g, '_').toUpperCase()}`] || '';
+    if (options && options.required && !val) {
+        throw new Error(`Input required and not supplied: ${name}`);
+    }
+    return val.trim();
+}
+exports.getInput = getInput;
+/**
+ * Sets the value of an output.
+ *
+ * @param     name     name of the output to set
+ * @param     value    value to store
+ */
+function setOutput(name, value) {
+    command_1.issueCommand('set-output', { name }, value);
+}
+exports.setOutput = setOutput;
+//-----------------------------------------------------------------------
+// Results
+//-----------------------------------------------------------------------
+/**
+ * Sets the action status to failed.
+ * When the action exits it will be with an exit code of 1
+ * @param message add error issue message
+ */
+function setFailed(message) {
+    process.exitCode = ExitCode.Failure;
+    error(message);
+}
+exports.setFailed = setFailed;
+//-----------------------------------------------------------------------
+// Logging Commands
+//-----------------------------------------------------------------------
+/**
+ * Writes debug message to user log
+ * @param message debug message
+ */
+function debug(message) {
+    command_1.issueCommand('debug', {}, message);
+}
+exports.debug = debug;
+/**
+ * Adds an error issue
+ * @param message error issue message
+ */
+function error(message) {
+    command_1.issue('error', message);
+}
+exports.error = error;
+/**
+ * Adds an warning issue
+ * @param message warning issue message
+ */
+function warning(message) {
+    command_1.issue('warning', message);
+}
+exports.warning = warning;
+/**
+ * Writes info to log with console.log.
+ * @param message info message
+ */
+function info(message) {
+    process.stdout.write(message + os.EOL);
+}
+exports.info = info;
+/**
+ * Begin an output group.
+ *
+ * Output until the next `groupEnd` will be foldable in this group
+ *
+ * @param name The name of the output group
+ */
+function startGroup(name) {
+    command_1.issue('group', name);
+}
+exports.startGroup = startGroup;
+/**
+ * End an output group.
+ */
+function endGroup() {
+    command_1.issue('endgroup');
+}
+exports.endGroup = endGroup;
+/**
+ * Wrap an asynchronous function call in a group.
+ *
+ * Returns the same type as the function itself.
+ *
+ * @param name The name of the group
+ * @param fn The function to wrap in the group
+ */
+function group(name, fn) {
+    return __awaiter(this, void 0, void 0, function* () {
+        startGroup(name);
+        let result;
+        try {
+            result = yield fn();
+        }
+        finally {
+            endGroup();
+        }
+        return result;
+    });
+}
+exports.group = group;
+//# sourceMappingURL=core.js.map
+
+/***/ }),
+
+/***/ 605:
+/***/ (function(module) {
+
+module.exports = require("http");
+
+/***/ }),
+
+/***/ 622:
+/***/ (function(module) {
+
+module.exports = require("path");
+
+/***/ }),
+
+/***/ 691:
+/***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
+
+const config = __webpack_require__(68);
+const {sendHttpRequest} = __webpack_require__(703);
+
+const core = __webpack_require__(470);
+
+const INPUT_KEY_NAME = 'name';
+const INPUT_KEY_VALUE = 'value';
+const INPUT_KEY_AUTH_TOKEN = 'token';
+const INPUT_KEY_IGNORE_FAILURE = 'ignore-failure';
+
+// most @actions toolkit packages have async methods
+async function run() {
+  try {
+    const name = core.getInput(INPUT_KEY_NAME);
+    const value = core.getInput(INPUT_KEY_VALUE);
+    const token = core.getInput(INPUT_KEY_AUTH_TOKEN) || process.env.GITHUB_TOKEN;
+    const ignoreFailure = core.getInput(INPUT_KEY_IGNORE_FAILURE) === 'true';
+    const currentRepo = process.env.GITHUB_REPOSITORY;
+
+
+    console.log(`:: Writing key: ${name} for repository: ${currentRepo}`);
+    const data = {value, name, action: 'set'};
+
+    const response = await sendHttpRequest(config.secretStoreUrl, 'POST', JSON.stringify(data), {
+      'x-github-repository': currentRepo,
+      'x-github-token': token,
+      'content-type': 'application/json'
+    });
+
+    if (response.statusCode !== 200) {
+      if (ignoreFailure) {
+        console.log('::Error:: ', response.statusCode, '=>', response.data);
+        core.setOutput('status', response.statusCode);
+      } else {
+        core.setFailed(JSON.parse(response.data));
+      }
+    } else {
+      core.setOutput('status', response.statusCode);
+    }
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
+
+/***/ }),
+
+/***/ 703:
+/***/ (function(__unusedmodule, exports, __webpack_require__) {
+
+const http = __webpack_require__(605);
+const https = __webpack_require__(211);
+const uriParser = __webpack_require__(835);
+const crypto = __webpack_require__(417);
+
+
+
+exports.sendHttpRequest = function (url, method = 'get', rawBody, headers = {}) {
+
+  const urlParts = uriParser.parse(url);
+  const client = urlParts.protocol === 'https:' ? https : http;
+  let port = urlParts.port;
+  if (!port) {
+    port = urlParts.protocol === 'https:' ? 443 : 80;
+  }
+
+  const options = {
+    hostname: urlParts.hostname,
+    port,
+    path: urlParts.path,
+    method: method.toUpperCase(),
+    headers: {Accept: '*/*', 'User-Agent': `NodeHttpLib/${process.version}`}
+  };
+
+  if (headers) {
+    for (let h in headers) {
+      options.headers[h] = headers[h];
+    }
+  }
+
+  return new Promise((resolve, reject) => {
+    let responseBody = '';
+    const req = client.request(options, function (res) {
+      console.log('STATUS:', res.statusCode);
+      res.setEncoding('utf8');
+
+      res.on('data', function (chunk) {
+        responseBody += chunk;
+      });
+
+      res.on('end', function () {
+        console.log('Request Completed');
+        resolve({
+          statusCode: res.statusCode,
+          data: responseBody,
+          headers: res.headers
+        });
+      });
+    });
+
+    req.on('error', function (e) {
+      console.log('Problem with request:', e.message);
+      e.data = responseBody;
+      reject(e);
+    });
+
+    if (rawBody) {
+      req.write(rawBody);
+    }
+    req.end();
+  });
+};
+
+exports.sha1Hex = (data) => crypto.createHash('sha1').update(data).digest('hex');
+exports.sha256Hex = (data) => crypto.createHash('sha256').update(data).digest('hex');
+
+/***/ }),
+
+/***/ 835:
+/***/ (function(module) {
+
+module.exports = require("url");
+
+/***/ })
+
+/******/ });
---
+++ write-secret/index.js
@@ -0,0 +1,46 @@
+const config = require('../config');
+const {sendHttpRequest} = require('../lib');
+
+const core = require('@actions/core');
+
+const INPUT_KEY_NAME = 'name';
+const INPUT_KEY_VALUE = 'value';
+const INPUT_KEY_AUTH_TOKEN = 'token';
+const INPUT_KEY_IGNORE_FAILURE = 'ignore-failure';
+
+// most @actions toolkit packages have async methods
+async function run() {
+  try {
+    const name = core.getInput(INPUT_KEY_NAME);
+    const value = core.getInput(INPUT_KEY_VALUE);
+    const token = core.getInput(INPUT_KEY_AUTH_TOKEN) || process.env.GITHUB_TOKEN;
+    const ignoreFailure = core.getInput(INPUT_KEY_IGNORE_FAILURE) === 'true';
+    const currentRepo = process.env.GITHUB_REPOSITORY;
+
+
+    console.log(`:: Writing key: ${name} for repository: ${currentRepo}`);
+    const data = {value, name, action: 'set'};
+
+    const response = await sendHttpRequest(config.secretStoreUrl, 'POST', JSON.stringify(data), {
+      'x-github-repository': currentRepo,
+      'x-github-token': token,
+      'content-type': 'application/json'
+    });
+
+    if (response.statusCode !== 200) {
+      if (ignoreFailure) {
+        console.log('::Error:: ', response.statusCode, '=>', response.data);
+        core.setOutput('status', response.statusCode);
+      } else {
+        core.setFailed(JSON.parse(response.data));
+      }
+    } else {
+      core.setOutput('status', response.statusCode);
+    }
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
